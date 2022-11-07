858355f68be7858eb125c15cf2bc4db069fe782e
Antoni Wojcik <zcicwoj@ucl.ac.uk>
2022-11-07 18:42:03 +0000

    Attempt at feature reduction

--- A1/a1module.py
+++ A1/a1module.py
@@ -5,6 +5,7 @@ import pandas as pd
 from sklearn.neural_network import MLPClassifier 
 from sklearn.model_selection import GridSearchCV
 from sklearn.linear_model import SGDClassifier
+from sklearn.decomposition import PCA
 from sklearn.model_selection import train_test_split
 from sklearn.svm import SVC
 from sklearn import metrics
@@ -28,13 +29,13 @@ img_count = len(lab_names)
 
 img_size = Image.open("./Datasets/celeba/img/" + lab_names[0]).size
 
-img_per_batch = 500
+img_per_batch = 5000
 num_batches = int(img_count / img_per_batch)
 
 # Apply GridSearchCV to find best parameters for given dataset
 # verbose is used to describe the steps taken to find best parameters
 #clf = SVC(gamma=0.001, kernel="rbf")
-clf = SGDClassifier(learning_rate = 'optimal', eta0 = 0.1, shuffle = False, )
+clf = SGDClassifier(learning_rate = 'optimal', eta0 = 0.1, shuffle = False)
 
 X_test = []
 y_test = []
@@ -55,9 +56,17 @@ for k in range(num_batches):
     img_data = []
 
     for i in range(img_per_batch):
-        img_i = Image.open("./Datasets/celeba/img/" + lab_names[k * img_per_batch + i]).convert('L') # Open images and convert to greyscale
+        img = Image.open("./Datasets/celeba/img/" + lab_names[k * img_per_batch + i]).convert('L') # Open images and convert to greyscale
+        #img = np.array(img).flatten()
+
+        pca_img = PCA(n_components = 50)
+
+        img_reduced = pca_img.fit_transform(img)
+        #img_recovered = pca_img.inverse_transform(img_reduced)
+
+        #plt.imshow(img_recovered, cmap='gray_r')
         
-        img_data.append(np.array(img_i).flatten())
+        img_data.append(img_reduced.flatten())
 
     X_train = []
     y_train = []
@@ -74,7 +83,7 @@ for k in range(num_batches):
         )
 
     # Learn the digits on the train subset
-    clf.partial_fit(X_train, y_train, classes=av_classes)
+    clf.fit(X_train, y_train)
 
 
 # Predict the value of the digit on the test subset
