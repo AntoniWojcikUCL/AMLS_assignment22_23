26ad34e2ee77fff534671fa0f61caf7d60ca575b
Antoni Wojcik <zcicwoj@ucl.ac.uk>
2022-11-12 19:44:03 +0000

    Improve the script to find mean eye color and save to csv

--- B2/selectEyes.py
+++ B2/selectEyes.py
@@ -40,42 +40,34 @@ params.filterByConvexity = False
 
 detector = cv2.SimpleBlobDetector_create(params)
 
-incorr_imgs = 0
 
-#for i in range(0, 9999):
-#
-#    img = cv2.imread(DATASET_PATH + '/img/' + labels[0])
-#
-#    keypoints = detector.detect(img)
-#
-#    if len(keypoints) != 2:
-#        incorr_imgs += 1
-#        break
-#
-#print(i)
+col_data = np.zeros((len(labels), 6))
 
-img = cv2.imread(DATASET_PATH + '/img/' + labels[3])
-img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+for i in range(len(labels)):
+    img = cv2.imread(DATASET_PATH + '/img/' + labels[i])
 
-keypoints = detector.detect(img)
+    keypoints = detector.detect(img)
 
-col_mean = np.zeros((4))
+    # Find average color and its mean std dev in the blobs
+    mask_circle = np.zeros(img.shape[:2], np.uint8)
 
-mask_empty = np.zeros(img.shape[:2], np.uint8)
+    for j in range(len(keypoints)):
+        pt = keypoints[j].pt
+        radius = keypoints[j].size / 2
+        mask_circle = cv2.circle(mask_circle, center = (int(pt[0]), int(pt[1])), radius = int(radius), color = (255, 255, 255), thickness = -1)
 
-for i in range(len(keypoints)):
-    pt = keypoints[i].pt
-    mask_circle = cv2.circle(mask_empty, center = (int(pt[0]), int(pt[1])), radius = int(keypoints[i].size), color = (255, 255, 255), thickness = -1)
+    mean, std = cv2.meanStdDev(img, mask = mask_circle)
 
-    col_mean += np.asarray(cv2.mean(img, mask = mask_circle))
+    col_data[i, 0:3] = mean[:, 0]
+    col_data[i, 3:6] = std[:, 0]
 
-col_mean = col_mean[0:3]
+df = pd.DataFrame(data = col_data, columns = ['B', 'G', 'R', 'S_B', 'S_G', 'S_R'])
 
-col_mean /= len(keypoints)
+df.to_csv('./B2/col_data.csv', sep = "\t")
 
-print(col_mean)
+# img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
-img_proc = cv2.drawKeypoints(img, keypoints, np.array([]), (0,255,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-
-cv2.imshow("Keypoints", img_proc)
-cv2.waitKey(0)
+# img_proc = cv2.drawKeypoints(img, keypoints, np.array([]), (255, 0, 255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+# 
+# cv2.imshow("Keypoints", img_proc)
+# cv2.waitKey(0)
